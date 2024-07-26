7c3947b9805d3738952002d4445732987f97144e
Jason <jason@JasondeMacBook-Air.local>
2024-07-26 09:45:41 +0800

    Two Sum

--- Two Sum.py
+++ Two Sum.py
@@ -13,8 +13,27 @@ class Solution(object):
                 if nums[j] == diff:
                     return [i, j]
 
+    def twoPassHashTwoSum(self, nums, target):
+        numMap = {}
+        for i in range(len(nums)):
+            numMap[nums[i]] = i
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in numMap.keys() and numMap[diff] != i:
+                return [i, numMap[diff]]
+        return []
+
+    def onePassHashTwoSum(self, nums, target):
+        numMap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in numMap.keys():
+                return [i, numMap[diff]]
+            numMap[nums[i]] = i
+        return []
+
 
 nums = [3, 2, 4]
 target = 6
 sol = Solution()
-print(sol.twoSum(nums, target))
+print(sol.twoPassHashTwoSum(nums, target))
