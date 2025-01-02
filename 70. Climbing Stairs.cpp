89b0ba18821795e5d0ae29eb94e9d8a3ffc45790
Jasperora <robert852654@gmail.com>
2025-01-02 21:34:08 +0800

    climbing stairs

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int ans[46];
+        ans[1] = 1;
+        ans[2] = 2;
+        for(int i = 3; i <= 45; ++i){
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        return ans[n];
+    }
+};
