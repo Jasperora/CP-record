c0531e5cb980fc870bfcebfe12a4c4271ab53bd6
Jasperora <robert852654@gmail.com>
2025-01-07 11:50:13 +0800

    maximum subarray sum dp

---
+++ maximum_subarray_sum-dp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int main(){
+    ll n;
+    cin >> n;
+    ll x[n];
+    for(int i = 0; i < n; ++i) cin >> x[i];
+    ll max_sub_sum_cur, ans;
+    max_sub_sum_cur = ans = x[0];
+    for(int i = 1; i < n; ++i){
+        if(x[i]+max_sub_sum_cur > x[i])
+            max_sub_sum_cur = x[i]+max_sub_sum_cur;
+        else max_sub_sum_cur = x[i];
+        if(max_sub_sum_cur>ans)
+            ans = max_sub_sum_cur;
+    }
+    cout << ans;
+}
