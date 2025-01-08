2409b0f1fdaf587a4ee49729b7d8bb5526469268
Jasperora <robert852654@gmail.com>
2025-01-08 16:00:51 +0800

    same tree

---
+++ 100. Same Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSame(TreeNode* root_a, TreeNode* root_b){
+        if(root_a==root_b&&!root_a) return true;
+        else if((!root_a&&root_b)||(root_a&&!root_b)) return false;
+        else{
+            if((root_a->left&&!root_b->left)||(!root_a->left&&root_b->left)) return false;
+            if((root_a->right&&!root_b->right)||(!root_a->right&&root_b->right)) return false;
+            bool same = true;
+            same &= root_a->val==root_b->val;
+            if(root_a->left)
+                same &= isSame(root_a->left, root_b->left);
+            if(root_a->right)
+                same &= isSame(root_a->right, root_b->right);
+            return same;
+        }
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return isSame(p, q);
+    }
+};
