3f9c42b7c4d237093cc450434f8a3d1c9454dde7
Jasperora <robert852654@gmail.com>
2025-01-11 09:18:04 +0800

    7

--- 7. Reverse Integer.cpp
+++ 7. Reverse Integer.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int reverse(int x) {
-        int ans = 0;
-        bool negative = x<0;
-        if(x>2147483647||x<-2147483647) return 0;
-        x = abs(x);
-        long long tmp;
-        while(x>0){
-            tmp = (long long)(ans)*10;
-            if(tmp>2147483647) return 0;
-            ans *= 10;
-            ans += x % 10;
-            x = (x-(x%10))/10;
+        int ret = 0;
+        // int INT_MIN = -2147483648;
+        // int INT_MAX = 2147483647;
+        bool negative = false;
+        if(x<0) negative = true;
+        if(negative){
+            if(x==INT_MIN) return 0;
+            else{
+                x *= -1;
+            }
         }
-        if(negative) ans *= -1;
-        return ans;
+        while(x){
+            int digit = x%10;
+            x /= 10;
+            if(ret>(int)INT_MAX/10)
+                return 0;
+            ret *= 10;
+            ret += digit;
+        }
+        if(negative) ret *= -1;
+        return ret;
     }
 };
