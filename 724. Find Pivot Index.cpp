2665c34d81de7922e36e9a9133a00aa4346cecf3
Jasperora <robert852654@gmail.com>
2025-01-18 23:16:48 +0800

    724 fix

--- 724. Find Pivot Index.cpp
+++ 724. Find Pivot Index.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-    int ret = 0;
-    vector<pair<int, int> > queen_pos;
-    void backtrack(int row, const int& n){
-        if(row==n){
-            ++ret;
-        }else{
-            for(int j = 0; j < n; ++j){
-                bool can_place = true;
-                for(auto& p : queen_pos){
-                    if(abs(p.first-row)==abs(p.second-j)||j==p.second){
-                        can_place = false;
-                        break;
-                    }
-                }
-                if(can_place){
-                    queen_pos.push_back({row, j});
-                    backtrack(row+1, n);
-                    queen_pos.pop_back();
-                }
-            }
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+        for(auto& n : nums) sum += n;
+        int tmp = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            if((tmp==(sum-nums[i])>>1)&&!((sum-nums[i])&1)){
+                return i;}
+            tmp += nums[i];
         }
-    }
-    int totalNQueens(int n) {
-        backtrack(0, n);
-        return ret;
+        return -1;
     }
 };
