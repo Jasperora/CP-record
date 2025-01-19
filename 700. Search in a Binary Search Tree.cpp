e833ea9baa74e97929de32843019738276a00129
Jasperora <robert852654@gmail.com>
2025-01-19 21:26:00 +0800

    700

---
+++ 700. Search in a Binary Search Tree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* ret = root;
+        while(ret&&ret->val!=val){
+            if(val<ret->val) ret = ret->left;
+            else ret = ret->right;
+        }
+        return ret;
+    }
+};
