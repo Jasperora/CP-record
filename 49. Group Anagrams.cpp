a1447e22307c22552e115a9a5a562fe220f9372e
Jasperora <robert852654@gmail.com>
2025-02-25 20:17:00 +0800

    49

--- 49. Group Anagrams.cpp
+++ 49. Group Anagrams.cpp
@@ -1,19 +1,15 @@
 class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<char, int> char_cnt[strs.size()];
-        map<map<char, int>, vector<string> > anagrams;
-        for(int i = 0; i < strs.size(); ++i){
-            for(auto& c : strs[i]){
-                if(!char_cnt[i].count(c)) char_cnt[i][c] = 0;
-                ++char_cnt[i][c];
+    public:
+        vector<vector<string>> groupAnagrams(vector<string>& strs) {
+            unordered_map<string, vector<string> > m;
+            for(auto& str : strs){
+                string tmp = str;
+                sort(tmp.begin(), tmp.end());
+                m[tmp].push_back(str);
             }
-            anagrams[char_cnt[i]].push_back(strs[i]);
+            vector<vector<string> > ret;
+            for(auto& p : m) ret.push_back(p.second);
+            return ret;
         }
-        vector<vector<string> > ans;
-        for(auto& p : anagrams){
-            ans.push_back(p.second);
-        }
-        return ans;
-    }
-};
+    };
+    
