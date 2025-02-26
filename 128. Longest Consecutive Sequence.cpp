358ff1b4ea16676dbef3a6dd818076daab26ee66
Jasperora <robert852654@gmail.com>
2025-02-26 20:27:50 +0800

    128

--- 128. Longest Consecutive Sequence.cpp
+++ 128. Longest Consecutive Sequence.cpp
@@ -1,22 +1,18 @@
 class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        if(nums.size()>1){
-            int a = nums[0], b = nums[0];
-            int max = 1;
-            for(int i = 1; i < nums.size(); ++i){
-                if(nums[i]==b+1){
-                    ++b;
-                    if(b-a+1>max) max = b-a+1;
-                }else if(nums[i] > b+1){
-                    a = nums[i];
-                    b = nums[i];
+    public:
+        int longestConsecutive(vector<int>& nums) {
+            unordered_set<int> s;
+            for(auto& n : nums) s.insert(n);
+    
+            int ret = 0;
+            for(auto& n : s){
+                if(!s.count(n-1)){
+                    int tmp = n + 1;
+                    while(s.count(tmp))
+                        ++tmp;
+                    ret = max(ret, tmp-n);
                 }
             }
-            return max;
-        }else{
-            return nums.size();
+            return ret;
         }
-    }
-};
+    };
