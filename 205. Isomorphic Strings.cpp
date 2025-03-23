35a604743629079294ec8e001779b5c21b6cd299
Jasperora <robert852654@gmail.com>
2025-03-23 09:36:47 +0800

    205

--- 205. Isomorphic Strings.cpp
+++ 205. Isomorphic Strings.cpp
@@ -1,22 +1,17 @@
 class Solution {
     public:
         bool isIsomorphic(string s, string t) {
+            if(s.size()!=t.size()) return false;
             unordered_map<char, vector<int>> m1, m2;
             for(int i = 0; i < s.size(); ++i){
                 m1[s[i]].push_back(i);
-            }
-            for(int i = 0; i < t.size(); ++i){
                 m2[t[i]].push_back(i);
             }
-            vector<vector<int>> vv1, vv2;
-            for(auto& p : m1){
-                vv1.push_back(p.second);
-            }
-            for(auto& p : m2){
-                vv2.push_back(p.second);
-            }
-            sort(vv1.begin(), vv1.end());
-            sort(vv2.begin(), vv2.end());
-            return vv1==vv2;
+            vector<vector<int>> v1, v2;
+            for(auto& p : m1) v1.push_back(p.second);
+            for(auto& p : m2) v2.push_back(p.second);
+            sort(v1.begin(), v1.end());
+            sort(v2.begin(), v2.end());
+            return v1==v2;
         }
     };
