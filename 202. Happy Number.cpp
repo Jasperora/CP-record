22aad04e882d5f20de28a67dd39fbc46ab205ed2
Jasperora <robert852654@gmail.com>
2025-03-25 22:48:25 +0800

    202

--- 202. Happy Number.cpp
+++ 202. Happy Number.cpp
@@ -1,18 +1,20 @@
 class Solution {
-public:
-    bool isHappy(int n) {
-        set<int> seen;
-        seen.insert(n);
-        while(n!=1){
-            long long next_n = 0;
-            while(n>0){
-                next_n += (long long)(n%10)*(n%10);
+    public:
+        int next(int n){
+            int ret = 0;
+            while(n){
+                ret += (n%10) * (n%10);
                 n /= 10;
             }
-            if(seen.count(next_n)) return false;
-            seen.insert(next_n);
-            n = next_n;
+            return ret;
         }
-        return true;
-    }
-};
+        bool isHappy(int n) {
+            int slow = next(n);
+            int fast = next(next(n));
+            while(fast!=1&&slow!=fast){
+                slow = next(slow);
+                fast = next(next(fast));
+            }
+            return fast==1;
+        }
+    };
