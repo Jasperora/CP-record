3991d5af7b08cb1b211440e19183137daf8e2a7a
Jasperora <robert852654@gmail.com>
2025-04-12 22:21:53 +0800

    63, 64

--- 63. Unique Paths II.cpp
+++ 63. Unique Paths II.cpp
@@ -1,23 +1,20 @@
 class Solution {
-public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        int dp[m][n];
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                if(obstacleGrid[i][j]) dp[i][j] = 0;
-                else{
-                    if(i==0&&j==0) dp[i][j] = 1;
-                    else{
-                        dp[i][j] = 0;
-                        if(i-1>=0) dp[i][j] += dp[i-1][j];
-                        if(j-1>=0) dp[i][j] += dp[i][j-1];
-                    }
+    public:
+        int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+            if(obstacleGrid[0][0]) return 0;
+            int m = obstacleGrid.size();
+            int n = obstacleGrid[0].size();
+            vector<int> dp(n, 0);
+            dp[0] = 1;
+            for(int i = 1; i < n; ++i){
+                if(!obstacleGrid[0][i]) dp[i] = dp[i-1];
+            }
+            for(int i = 1; i < m; ++i){
+                for(int j = 0; j < n; ++j){
+                    if(obstacleGrid[i][j]) dp[j] = 0;
+                    else if(j >= 1) dp[j] += dp[j-1];
                 }
             }
+            return dp[n-1];
         }
-        
-        return dp[m-1][n-1];
-    }
-};
+    };
